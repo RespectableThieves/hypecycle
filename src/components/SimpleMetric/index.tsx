04657f02f67581ac33eb9a5d0ba345cfa357bdb4
Shaun Mulligan <switchflip.shaun@gmail.com>
2023-02-25 11:34:42 +0100

    fix path to materialCommunityIcons

--- src/components/SimpleMetric/index.tsx
+++ src/components/SimpleMetric/index.tsx
@@ -3,7 +3,7 @@ import { Text } from 'react-native-paper';
 import { CenteredView,DataText,OuterView, Title } from './styles'
 import { Dimensions, View } from 'react-native';
 import { useHeaderHeight } from '@react-navigation/elements';
-import MaterialCommunityIcons from '@expo/vector-icons/build/MaterialCommunityIcons';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 type Props = {
   title: string;
