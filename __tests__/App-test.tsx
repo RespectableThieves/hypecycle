ca5817a9695097f1de6c501c20ca9ea34aff8b25
Craig Mulligan <git@craigmulligan.com>
2023-03-18 17:24:47 -0400

    add global setup file

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -1,32 +1,8 @@
 /**
  * @format
  */
-
-import 'react-native';
-import 'react-native-gesture-handler/jestSetup';
-import React from 'react';
 import App from '../App';
-
-// Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
-jest.mock('react-native-cycling-sensors');
-jest.mock('expo-font');
-jest.mock('expo-asset');
-jest.mock('react-native-reanimated', () => {
-  const Reanimated = require('react-native-reanimated/mock');
-
-  // The mock for `call` immediately calls the callback which is incorrect
-  // So we override it with a no-op
-  Reanimated.default.call = () => { };
-
-  return Reanimated;
-});
-
-// Silence the warning: Animated: `useNativeDriver` is not supported because the native animated module is missing
-// jest.mock('react-native/Libraries/Animated/src/NativeAnimatedHelper');
-
-// As of react-native@0.64.X file has moved
-jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
 
 it('renders correctly', () => {
   const tree = renderer.create(<App />);
