0c88bdc8744bccda66de2c77649ef78e105404b1
Craig Mulligan <git@craigmulligan.com>
2023-03-18 17:12:21 -0400

    nearly nearly there

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,11 +1,23 @@
-import {Database} from '@nozbe/watermelondb';
-import SQLiteAdapters from '@nozbe/watermelondb/adapters/sqlite';
+import { Database } from '@nozbe/watermelondb';
+// import SQLiteAdapters from '@nozbe/watermelondb/adapters/sqlite';
+import LokiJSAdapter from '@nozbe/watermelondb/adapters/lokijs'
 
-import {schemas} from './schemas';
+
+import { schemas } from './schemas';
 import SensorModel from './model/sensorModel';
 
-const adapter = new SQLiteAdapters({
+// const adapter = new LokiJSAdapter({
+//   schema: schemas,
+//   useWebWorker: false,
+//   useIncrementalIndexedDB: false,
+// });
+const adapter = new LokiJSAdapter({
   schema: schemas,
+  useWebWorker: false,
+  useIncrementalIndexedDB: true,
+  extraLokiOptions: {
+    autosave: false
+  }
 });
 
 export const dataBase = new Database({
