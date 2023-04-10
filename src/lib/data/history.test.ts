fdff849bfa37150b6e79315143d92688a6fb2670
Craig Mulligan <git@craigmulligan.com>
2023-04-10 11:29:02 -0700

    check we are writing cadence and power to trackpoint

--- src/lib/data/history.test.ts
+++ src/lib/data/history.test.ts
@@ -1,28 +1,34 @@
-import {startRide, stopRide, saveRideSummary} from '../ride';
+import { startRide, stopRide, saveRideSummary } from '../ride';
 import {
   updateRealTimeRecordRandom,
   getOrCreateRealtimeRecord,
   onSnapshotEvent,
 } from './realtime';
-import {generateTCX} from './history';
-import {TrainingCenterDatabase} from 'tcx-builder';
+import { generateTCX } from './history';
+import { TrainingCenterDatabase } from 'tcx-builder';
 
 it('generate a tcx from ride summary', async () => {
+  jest.useFakeTimers()
   const snapshotCount = 5;
   const realtime = await getOrCreateRealtimeRecord();
   const ride = await startRide();
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   for (let _x in [...Array(snapshotCount).keys()]) {
-    await onSnapshotEvent();
+    jest.advanceTimersByTime(3000)
     await updateRealTimeRecordRandom(realtime);
+    await onSnapshotEvent();
   }
   await stopRide(ride);
   const rideSummary = await saveRideSummary(ride);
   const tcx = await generateTCX(rideSummary);
 
   expect(tcx).toBeInstanceOf(TrainingCenterDatabase);
-  expect(tcx.Activities?.Activity![0].Laps[0].Track?.TrackPoints.length).toBe(
+  const trackPoints = tcx.Activities?.Activity![0].Laps[0].Track!.TrackPoints!
+  expect(trackPoints.length).toBe(
     snapshotCount,
   );
-});
+
+  expect(trackPoints[trackPoints.length - 1].Cadence).toBe(realtime.cadence)
+  expect(trackPoints[trackPoints.length - 1].Extensions?.Watts).toBe(realtime.instantPower)
+}, 20000);
