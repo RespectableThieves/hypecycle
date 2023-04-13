cfbe572675c6bd6adb7bcaff16de7aa8e88ae94f
Craig Mulligan <git@craigmulligan.com>
2023-04-13 13:09:09 -0700

    more configs

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,9 @@
-module.exports = function (api) {
+module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      ['@babel/plugin-proposal-decorators', {legacy: true}],
+      ['@babel/plugin-proposal-decorators', { legacy: true }],
       'react-native-paper/babel',
       'react-native-reanimated/plugin',
     ],
